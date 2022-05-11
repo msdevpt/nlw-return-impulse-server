053923d86258c51cf8c8dd4e48b0d92a143d2065
M.Silva <msilva.kb@outlook.com>
2022-05-11 21:43:19 +0100

    feat: add try catch in routes

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ export const routes = express.Router();
 routes.post('/feedbacks', async(req, res) => {
   const { type, comment, screenshot } = req.body;
 
+try {
   const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
   const nodemailerMailAdapter = new NodemailerMailAdapter();
   const submitFeedbackUseCase = new SubmitFeedbackUseCase(
@@ -18,6 +19,9 @@ routes.post('/feedbacks', async(req, res) => {
   await submitFeedbackUseCase.execute({ 
     type, comment, screenshot 
   });
+} catch (error) {
+  console.error(error);
+}
  
   res.status(201).send();
 });
